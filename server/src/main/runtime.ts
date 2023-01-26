bcbd5e649742bcdec385ab5363f6891ef9f94712
Harvtronix Webmaster <webmaster@harvtronix.com>
2023-01-26 01:19:36 -0500

    read actual inverter paths from env

--- server/src/main/runtime.ts
+++ server/src/main/runtime.ts
@@ -2,9 +2,16 @@ import { NodeEnv } from './interfaces.js'
 
 class Runtime {
   public readonly nodeEnv: NodeEnv
+  public readonly inverterPaths: Array<string>
 
   public constructor() {
     this.nodeEnv = process.env.NODE_ENV === 'production' ? NodeEnv.Production : NodeEnv.Development
+
+    // For example: INVERTER_PATHS=/dev/ttyUSB0,/dev/ttyUSB1
+    this.inverterPaths = process.env.INVERTER_PATHS ? process.env.INVERTER_PATHS.split(',') : []
+
+    console.log('nodeEnv:      ', this.nodeEnv)
+    console.log('inverterPaths:', this.inverterPaths)
   }
 }
 
