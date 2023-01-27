bcd4d74e8fa15318427ed9e83499e2d5b7aa1d47
Harvtronix Webmaster <webmaster@harvtronix.com>
2023-01-26 23:10:03 -0500

    log cleanup

--- server/src/main/db.service.ts
+++ server/src/main/db.service.ts
@@ -38,19 +38,3 @@ export class DbService {
     this.db.write()
   }
 }
-
-// const results = db.data.readings
-//   .filter((reading) => reading.timestamp >= 1650601226834)
-//   .sort((a, b) => a.timestamp - b.timestamp)
-
-// console.log(
-//   results.map((reading) => ({
-//     time: reading.timestamp,
-//     outputType: reading.holdingRegisters[HoldingRegister.OutputVoltType],
-//     batteryVoltage: reading.inputRegisters[InputRegister.BatteryVolt] / 100,
-//     outputWatts: Number.parseInt(
-//       reading.inputRegisters[InputRegister.OutputActivePowerHigh].toString() +
-//         reading.inputRegisters[InputRegister.OutputActivePowerLow].toString()
-//     )
-//   }))
-// )
--- server/src/main/sigineer-inverter.ts
+++ server/src/main/sigineer-inverter.ts
@@ -77,7 +77,6 @@ class SigineerInverter {
     try {
       // Read each of the registers within the range
       for (const range of registerRanges[registerType]) {
-        console.log(range)
         const response = await readFunction(range[0], range[1])
 
         let curRegister = range[0]
